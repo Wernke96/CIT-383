e960538e12cf621c0ad6995e206a6c16433b9f21
jerred <jerred@lamproslabs.com>
2019-02-11 19:05:27 -0500

    this won't add negative numbers

--- Project1.rb
+++ Project1.rb
@@ -43,13 +43,23 @@ while(menu != 0) do
     if (menu == 1 )
             print("Please Enter item cost: ");
             id[count] = gets().to_f();
+            if(id[count] < 0 )
+                puts("you can not add a number more then one")
+                break;
+            end
             id[count] = id[count].round(2);
+            
             count +=1;
+            
     end 
     if (menu == 2 && labor == 0)
             print("What the labor cost: ");
             labor += 1
             labor_cost = gets().to_f();
+            if(labor_cost < 0 )
+                puts("you can not add a number more then one")
+                break;
+            end
             labor_cost = labor_cost.round(2);
     elsif (labor!= 0)
         puts("labor cost was already enter");
@@ -59,17 +69,24 @@ while(menu != 0) do
 
             discount_exist += 1;
             discount=gets().to_i();
+            if(discount < 0 )
+                puts("you can not add a number more then one")
+                break;
+            end
             
     elsif (discount_exist != 0 ) 
             puts("You add a discount code all ");
        end 
         if(menu == 4)
             gift_card[j]= gets.to_f();
+            if(gift_card[j] < 0 )
+                puts("you can not add a number more less then 0");
+                break;
             gift_card[j] = gift_card[j].round(2);
             j+=1;
         end
         if (menu==5)
-            cal(gift_card,id,labor,discount);
+            cal(gift_card,id,labor_cost,discount);
         end
         if (menu == 9 )
             puts("Please enter new Customer data ");
@@ -80,6 +97,9 @@ while(menu != 0) do
             gift_card.clear();
             j = 0
             id.clear();
+        end
+        if (menu < 0 )
+            puts("Invalid number try again");
         end
 end
  puts("Thank you for us our application");
